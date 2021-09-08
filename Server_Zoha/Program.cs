b45a2ce33c1ee25721847d2147b0f6f9b3b9a754
Zoha <student@05-03>
2021-09-08 11:23:45 +0300

    Create Server

--- Server_Zoha/Program.cs
+++ Server_Zoha/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server_Zoha
 {
     class Program
     {
+        static int port = 8000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Start server...");
+            try
+            {
+                socket.Bind(iPEndPoint);
+                socket.Listen(10);
+
+                while (true)
+                {
+                    Socket socketClient = socket.Accept();
+
+                    socketClient.Send(Encoding.Unicode.GetBytes("Welcome on server!"));
+
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do
+                    {
+                        bytes = socketClient.Receive(data);
+                        stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socketClient.Available > 0);
+
+                    Console.WriteLine($"MSG: {stringBuilder.ToString()}");
+
+
+                    socketClient.Shutdown(SocketShutdown.Both);
+                    socketClient.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
