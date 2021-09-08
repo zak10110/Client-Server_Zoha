ce6f306fa36bb06eba10190f04b03107a115cf89
Zoha <student@05-03>
2021-09-08 11:25:38 +0300

    Create Client

--- Client_Zoha/Program.cs
+++ Client_Zoha/Program.cs
@@ -1,11 +1,47 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client_Zoha
 {
     class Program
     {
+        static string ipAddr = "127.0.0.1";
+        static int port = 8000;
         static void Main(string[] args)
         {
+            try
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ipAddr), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(iPEndPoint);
+
+                int bytes = 0;
+                byte[] data = new byte[256];
+                StringBuilder stringBuilder = new StringBuilder();
+
+                do
+                {
+                    bytes = socket.Receive(data);
+                    stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+                Console.WriteLine(stringBuilder.ToString());
+
+
+                Console.Write("Enter msg for server:");
+                string sms = Console.ReadLine();
+                data = Encoding.Unicode.GetBytes(sms);
+
+                socket.Send(data);
+
+                Console.WriteLine($"Sms \"{sms}\" send to SERVER [{ipAddr}]!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.WriteLine("Hello World!");
         }
     }
